9cc9058d6af2a15bd0353b3ff3671c5616c974d8
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-21 04:58:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,13 @@
 class Pokemon
   
   attr_accessor :file, :parsed_file, :all_pokemon, :db
-  attr_reader :id
   
-    def initialize(name, type, db, id = nil)
-      @id = id
-      @name = name
-      @type = type
-      @db = db
+  
+    def initialize(db)
+      self.db = db
+      self.file = file.open("pokemon_index.html")
+      self.parsed_file = Nokogiri::HTML.parse(file)
+      self.all_pokemon = catch_em_all
     end
     
     def self.save
