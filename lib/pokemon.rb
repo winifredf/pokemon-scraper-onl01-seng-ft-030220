502082d00e53dab69fa82d92fc0428d03087932a
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-21 05:01:56 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,10 @@
 class Pokemon
   
+  attr_accessor :name, :type, :db
+  attr_reader :id
   
+    def initalize(keywords)
+      @id, @name, @type, @db = keywords.values[0], keywords.values[1], keywords.values[2], keywords.values[3]
     end
     
     def self.save
