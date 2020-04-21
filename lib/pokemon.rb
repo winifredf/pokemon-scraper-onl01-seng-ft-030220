4700636f7b722ebe1babc6a6434df92613c88235
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-21 05:04:26 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :name, :type, :db
   attr_reader :id
   
-    def initalize(name:, type:, db:, id:)
+    def initalize(name:, type:, db:, id: nil)
       @id = id
       @name = name 
       @type = type
