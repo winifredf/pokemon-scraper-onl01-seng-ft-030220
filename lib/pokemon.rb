be68dc8e7ff3ae4860375702f650b7047e6e0342
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-21 05:09:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,7 +7,8 @@ class Pokemon
       @id, @name, @type, @db = keywords.values[0], keywords.values[1], keywords.values[2], keywords.values[3]
     end
     
-    def self.save
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
       
     end
 end
